9cbacb883e062fc61859b980c23732e87d3adecf
HONGKYUMIN (ANTHONY) <hisp1301@gmail.com>
2022-06-13 10:45:21 +0900

    추가) Java 클래스 만들기 정석 예제

---
+++ src/main/java/com/webmarket/domain/model/Product.java
@@ -0,0 +1,129 @@
+package com.webmarket.domain.model;
+
+import java.util.Objects;
+
+public class Product extends Object {
+	
+	public static void main(String[] args) {
+		Product product = new Product("33", "33", 11);
+		
+		Product product2 = new Product("33", "33", 11);
+		
+		Object object = new Product("33", "33", 11);
+
+		System.out.println(product.equals(product2));	// true
+		System.out.println(product == product2);	// false
+		
+	}
+	
+	private String id;
+	private String name;
+	private int unitPrice;
+	private String description;
+	private String manufacturer;
+	private String category;
+	private long unitsInStock;
+	private String condition;
+	
+	public Product(String id, String name, int unitPrice) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public long getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(long unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", unitPrice=" + unitPrice + ", description=" + description
+				+ ", manufacturer=" + manufacturer + ", category=" + category + ", unitsInStock=" + unitsInStock
+				+ ", condition=" + condition + "]";
+	}
+	
+	
+}
+
